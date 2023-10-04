25f6a510ce0fe53abdd441b0cc93aac8c4c66696
youssef Charafeddine <yosuke2017@hotmail.com>
2023-10-04 11:13:26 +0200

    feat: add initial boilerplate

---
+++ src/__tests__/sum.test.js
@@ -0,0 +1,8 @@
+// sum.test.js
+/* eslint-env node */
+
+const sum = require('../sum');
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3);
+});
---
+++ src/sum.js
@@ -0,0 +1,7 @@
+/* eslint-env node */
+
+function sum(a, b) {
+    return a + b;
+  }
+  
+  module.exports = sum;
